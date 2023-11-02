db9908eac309c0281684756300d04728ab4c51be
Lee-Yeongil <yonggil0313@gmail.com>
2023-11-02 14:53:33 +0900

    1102

--- Skill/Skill/MagicProjectile.cs
+++ Skill/Skill/MagicProjectile.cs
@@ -35,7 +35,6 @@ public class MagicProjectile : Hit_Skill
     {
         if (!PlayerOrBoss)
         {
-
             if (collision.gameObject.layer == 9)
             {
                 if (collision.gameObject.GetComponent<Monster>())
